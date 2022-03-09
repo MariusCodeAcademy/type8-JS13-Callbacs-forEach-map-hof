d68f4e5b94dcd9eacf31984d40056c419174d475
Marius <marius@example.com>
2022-03-09 10:18:07 +0200

    extra uzd

--- js/forEUzd.js
+++ js/forEUzd.js
@@ -4,4 +4,36 @@ const skArray = [0, 10, 15, -2, 30, -15, 10, -50];
 
 //1. consoleje atspaustinti kiekviena reiksme
 
-//
+//2. atspaustinti visu skaiciu suma
+
+// 3. atspausdinti tik teigiamus skaicius
+
+// 4. grazinti nauja masyva su neigiamais skaiciais
+
+// 5. grazinti nauja masyva kur visos neigiamos reiksmes paverstos teigiamom
+
+// 6. grazinti koks yra -15 skaiciaus indexas
+
+// 7. pakeisti -2 i -25 tame paciame masyve
+
+const arr2 = [
+  [0, 15, 25],
+  [15, -5, 30],
+];
+
+// extra sunkesnis
+// e1. is skArray su reduce grazinti vidurki
+// e2. is skArray su reduce grazinti objekta 
+{
+  vidurkis: ''
+}
+// e2. is skArray su reduce grazinti objekta 
+{
+  vidurkis: ''
+  totalSuma: ''
+}
+// e2. is skArray su reduce grazinti objekta 
+{
+  teigiami: [...]
+  neigiami: [...]
+}
